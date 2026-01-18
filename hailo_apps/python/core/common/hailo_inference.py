5c26d8254473824fabc236da4e72b970ba4a0ef8
Majed Abu Mokh <majedam@hlil-550-lap.qb.hailotech>
2026-01-18 16:52:38 +0200

    fix integrations issues

--- hailo_apps/python/core/common/hailo_inference.py
+++ hailo_apps/python/core/common/hailo_inference.py
@@ -6,11 +6,6 @@ import numpy as np
 
 from hailo_platform import (HEF, VDevice,FormatType, HailoSchedulingAlgorithm)
 from hailo_platform.pyhailort.pyhailort import FormatOrder
-import yaml
-from hailo_apps.config.config_manager import get_main_config
-from hailo_apps.python.core.common.defines import HAILO8_ARCH, HAILO8L_ARCH, CONFIG_ENABLED, CONFIG_DISABLED, DEFAULT_CONFIG_PATH
-from hailo_apps.python.core.common.installation_utils import detect_hailo_arch
-
 
 
 class HailoInfer:
@@ -29,15 +24,10 @@ class HailoInfer:
             output_type (Optional[str], optional): Output data type format. Common values: 'UINT8', 'UINT16', 'FLOAT32'.
             priority (optional[int]): Scheduler priority value for the model within the shared VDevice context. Defaults to 0.
         """
-        arch = detect_hailo_arch()
-        config = get_main_config()
-        multi_processing = config.get('multi_processing')
         params = VDevice.create_params()
         # Set the scheduling algorithm to round-robin to activate the scheduler
         params.scheduling_algorithm = HailoSchedulingAlgorithm.ROUND_ROBIN
         params.group_id = "SHARED"
-        if arch in [HAILO8_ARCH, HAILO8L_ARCH] and multi_processing == CONFIG_ENABLED:
-            params.multi_process_service = True
         vDevice = VDevice(params)
 
         self.target = vDevice
@@ -55,8 +45,6 @@ class HailoInfer:
         self.configured_model.set_scheduler_priority(priority)
         self.last_infer_job = None
 
-        if arch in [HAILO8_ARCH, HAILO8L_ARCH] and multi_processing == CONFIG_DISABLED:
-            self.configured_model.activate()
 
     def _set_input_type(self, input_type: Optional[str] = None) -> None:
         """
